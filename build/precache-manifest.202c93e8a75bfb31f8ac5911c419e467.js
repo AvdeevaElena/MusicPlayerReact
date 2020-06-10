239898106558affe88d0bc9e307db16e0a4ed07c
avdeevaelena5@gmail.com <avdeevaelena5@gmail.com>
2020-06-10 23:16:04 +0300

    ghb

---
+++ build/precache-manifest.202c93e8a75bfb31f8ac5911c419e467.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b3ac9ca0fd14530d53bcf87bc09ff7",
+    "url": "/MusicPlayerReact/index.html"
+  },
+  {
+    "revision": "95a39fd38d63de81b8b4",
+    "url": "/MusicPlayerReact/static/css/main.058cb6c0.chunk.css"
+  },
+  {
+    "revision": "4895288941bf967538b0",
+    "url": "/MusicPlayerReact/static/js/2.eda32ad2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MusicPlayerReact/static/js/2.eda32ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a39fd38d63de81b8b4",
+    "url": "/MusicPlayerReact/static/js/main.246eec27.chunk.js"
+  },
+  {
+    "revision": "eb45b4a441bfa6ab3a50",
+    "url": "/MusicPlayerReact/static/js/runtime-main.34e43adb.js"
+  },
+  {
+    "revision": "736d71960e5309583cf37c822eaf7ae8",
+    "url": "/MusicPlayerReact/static/media/Bo Kaspers Orkester - En sl ndas andetag.736d7196.mp3"
+  },
+  {
+    "revision": "2c0650686b14575101a7eec2d246ecd0",
+    "url": "/MusicPlayerReact/static/media/Israel Kamakawiwoole - What A Wonderful World.2c065068.mp3"
+  },
+  {
+    "revision": "e9339edda58885b80558337766f8f13c",
+    "url": "/MusicPlayerReact/static/media/Madness Mdness - Our House.e9339edd.mp3"
+  },
+  {
+    "revision": "f085a0f5d332cb15d20b162e5818429a",
+    "url": "/MusicPlayerReact/static/media/Martin Fierro - Coplas Telúricas.f085a0f5.mp3"
+  },
+  {
+    "revision": "00dc12006c6ff8ed29db2be33b1dc234",
+    "url": "/MusicPlayerReact/static/media/Rapossa - Burlesque.00dc1200.mp3"
+  },
+  {
+    "revision": "d47a2cdfb2aed99ad4324a1e73d0b8b8",
+    "url": "/MusicPlayerReact/static/media/Sufjan Stevens - Mystery of Love.d47a2cdf.mp3"
+  },
+  {
+    "revision": "0c25d176920e224041e80541eda6c4f5",
+    "url": "/MusicPlayerReact/static/media/back7.0c25d176.jpg"
+  }
+]);
